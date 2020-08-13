25c54a6636a69037de75e952816d2ad90a326c08
Darina Kukarina <kukarina.darina42@gmail.com>
2020-08-13 19:53:14 -0400

    Fix errors

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,7 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, presence: true
-  has_many :reviews
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian)}
+  has_many :reviews, dependent: :destroy
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true
   validates :rating, inclusion: { in: (0..5), allow_nil: false }
+  validates :rating, numericality: { only_integer: true }
   belongs_to :restaurant
 end
